46cae4ea860d006d04384a0f7fdd19f4fa371a78
sernst <swernst@gmail.com>
2016-05-08 09:02:43 -0500

    AWS Lambda and API Gateway example code

---
+++ Lambda_API_Examples/lambda_examples/example_3.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import re
+import string
+
+
+def to_integer(character):
+    """ Converts an ascii character to its alphabetical index integer """
+
+    if character in string.digits:
+        return int(character)
+
+    return string.ascii_lowercase.index(character.lower())
+
+
+def lambda_handler(event, context):
+    """ AWS Lambda function call """
+
+    try:
+        # Remove any characters that are not digits 0-9
+        phone_number = re.sub(
+            r'[^0-9]+',
+            '',
+            event['number']
+        )
+
+        phone_total = sum(map(to_integer, phone_number))
+        code_total = sum(map(to_integer, event['code']))
+
+        print('PHONE:', event['number'], '->', phone_total)
+        print('CODE:', event['code'], '->', code_total)
+
+        result = bool(phone_total == code_total)
+    except Exception:
+        raise Exception('ERROR::500')
+
+    return {'result': result}
---
+++ Lambda_API_Examples/lambda_examples/test/test_example_1.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import unittest
+
+from lambda_examples.example_1 import lambda_handler
+
+
+class TestExample1(unittest.TestCase):
+
+    def test_valid(self):
+        """
+        -> Pass matching number and code arguments
+        <- Should return true
+        """
+
+        arguments = dict(
+            number='(541) 555-4842',
+            code='ZS'
+        )
+        self.assertTrue(lambda_handler(arguments, None)['result'])
+
+    def test_invalid(self):
+        """
+        -> Pass mismatched number and code arguments
+        <- Should return false
+        """
+
+        arguments = dict(
+            number='612.555.0123',
+            code='ABCDEFG'
+        )
+        self.assertFalse(lambda_handler(arguments, None)['result'])
+
+    def test_illegal_character(self):
+        """
+        -> Pass code with illegal character
+        <- Should raise exception
+        """
+
+        arguments = dict(
+            number='612.555.0123',
+            code='A@B'
+        )
+
+        with self.assertRaises(ValueError):
+            lambda_handler(arguments, None)
+
+
+################################################################################
+################################################################################
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestExample1)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+
+
